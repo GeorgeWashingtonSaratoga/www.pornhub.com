27a01549669de182e95039e2b1c38ba42aefa8b6
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2023-03-07 17:21:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ if(document.getElementById("quandale").clicked == true)
 {
    window.location.href = "https://georgewashingtonsaratoga.github.io/quandale/";
 }
-
 if(document.getElementById("suirrel").clicked == true)
 {
    window.location.href = "https://georgewashingtonsaratoga.github.io/suirrelStory/";
 }
+
+var anus;

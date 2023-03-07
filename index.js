e693f6c4e233e3a41b8ad9d8ac270b30d24e95fb
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2023-03-07 14:12:33 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+if(document.getElementById('quandale').clicked == true)
+{
+   window.location.href = "https://github.com/GeorgeWashingtonSaratoga/quandale";
+}
+
+if(document.getElementById('suirrel').clicked == true)
+{
+   window.location.href = "https://github.com/GeorgeWashingtonSaratoga/suirrel";
+}

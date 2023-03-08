420dbae5dc210e59447034d43950b02c2b7cbe18
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2023-03-08 08:46:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var butt = document.getElementById("quandale");
-image.onclick = function(e) {
+butt.onclick = function(e) {
     alert("QUANDALE");
     window.location.href = "https://georgewashingtonsaratoga.github.io/quandale/";
 };
 
 var butt = document.getElementById("suirrel");
-image.onclick = function(e) {
+butt.onclick = function(e) {
     alert("SUIRREL");
     window.location.href = "https://georgewashingtonsaratoga.github.io/suirrelStory/";
 };
+
+var ANUS;
